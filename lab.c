e24dfe99ac688f548b47a6b4ae0f92c156cd7d7f
rameeltanveer <52344310+rameeltanveer@users.noreply.github.com>
2019-06-29 13:12:09 +0500

    Add files via upload

---
+++ lab.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+int main(){
+/*
+printf("kjsfnjlfjrf\n");
+printf("aaefef\n");
+*/
+
+/*
+short s=125;
+printf("%d",s);
+printf("\n");
+
+char c='c';
+printf("%d",c);
+signed char cc='2';
+printf("%c",cc);
+
+printf("\n");
+
+float f=33.33333333333333;
+printf("%n",f);
+
+printf("\n");
+*/
+
+
+int a;
+scanf("%d",&a);
+printf("your i/p is %d",a);
+
+}
